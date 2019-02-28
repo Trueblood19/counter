3335fc64ace2547d6a47e08f9e9110a2d17b4d92
Andrea <comeawaywithandrea@gmail.com>
2019-02-27 20:45:13 -0500

    you added counter functionality increment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class Counter extends Component {
+constructor () {
+    super();
+    this.state ={
+      count: 0,
+    };
+
 }
 
-export default App;
+
+increment = () => {
+  this.setState({
+    count: this.state.count + 1,
+  })
+}
+render (){
+  return(
+    <div className = "container">
+    <div className = "navbar">Counter.js</div>
+    <div className = "counter">
+    <h1> {this.state.count}</h1>
+    <button type="button" onClick = {this.increment}> Increment</button>
+    <button type="button" onClick = {this.decrement}> Decrement</button>
+    </div></div>
+  );
+}
+}
+export default Counter;
